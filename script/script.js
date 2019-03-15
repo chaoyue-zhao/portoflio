7f010b8b1099f55e68b5b597f176989036f123db
Chaoyue <chaoyue.zhao.2011@gmail.com>
2019-03-15 14:07:59 -0400

    chao built the header and about me section :octocat:

--- script/script.js
+++ script/script.js
@@ -1,14 +1,25 @@
-const Chao = {};
+const chao = {};
 
-Chao.greeting = () => {
-  document.querySelector('.greeting')
-}
+chao.greeting = () => {
+  const date = new Date
+  const time = date.getHours();
 
-Chao.init() = () => {
-  Chao.greeting();
-}
+  if (time >= 5 && time <= 11){
+    document.querySelector('.greeting').innerHTML = 'Morning';
+  } else if (time >=12 && time <= 16){
+    document.querySelector('.greeting').innerHTML = 'Afternoon';
+  } else if (time >=17 && time <= 22) {
+    document.querySelector('.greeting').innerHTML = 'Evening';
+  } else if ((time >=22 && time <= 24) || time < 4){
+    document.querySelector('.greeting').innerHTML = 'Night';
+  }
+};
+
+chao.init = () => {
+  chao.greeting();
+};
 
 (function () {
-  Chao.init();
+  chao.init();
 })();
 
