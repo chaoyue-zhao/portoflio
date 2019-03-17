3c14f3a896d6790dcf0d0882f0b6630285c44aa0
Chaoyue <chaoyue.zhao.2011@gmail.com>
2019-03-17 12:05:00 -0400

    Chao added some Slide Up effect :scroll:

--- script/script.js
+++ script/script.js
@@ -15,8 +15,32 @@ chao.greeting = () => {
   }
 };
 
+chao.parallax = () => {
+
+  // Init ScrollMagic
+  let controller = new ScrollMagic.Controller();
+
+  // get all triggers
+  const triggers = ["#slide-1", "#slide-2", "#slide-3", "#slide-4"];
+
+  // create scenes for each of the triggers
+  triggers.forEach(function (trigger, index) {
+
+    // number for highlighting scenes
+    const num = index + 1;
+    
+    // make scene
+    const triggerScene = new ScrollMagic.Scene({
+      triggerElement: trigger,
+      offset: -95
+    })
+      .setClassToggle("#slide-" + num, "is-active")
+      .addTo(controller);
+ })
+}
 chao.init = () => {
   chao.greeting();
+  chao.parallax();
 };
 
 (function () {
