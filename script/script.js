b5b1aba5102a5bcdc10749fbfe585199f4d26f26
Chaoyue <chaoyue.zhao.2011@gmail.com>
2019-03-29 11:33:08 -0400

    added loading screen fixed the header

--- script/script.js
+++ script/script.js
@@ -32,6 +32,74 @@ chao.carousel = () => {
   });
 }
 
+chao.loading = () => {
+/**************************************************************
+  THIS LOVELY BLOGPOST ON PRE-LOADING FROM PETR TICHY SAVED MY LIFE
+  CREDIT HERE: https://ihatetomatoes.net/a-simple-one-page-template-with-a-preloading-screen/
+**************************************************************/
+
+  // number of loaded images for preloader progress
+  let loadedCount = 0; //current number of images loaded
+  const imagesToLoad = $('.bcg').length; //number of slides with .bcg container
+  let loadingProgress = 0; //timeline progress - starts at 0
+
+  $('.bcg').imagesLoaded({
+    background: true
+  }).progress(function (instance, image) {
+    loadProgress();
+  });
+
+  function loadProgress(imgLoad, image) {
+    //one more image has been loaded
+    loadedCount++;
+
+    loadingProgress = (loadedCount / imagesToLoad);
+
+    // GSAP tween of our progress bar timeline
+    TweenLite.to(progressTl, 0.7, { progress: loadingProgress, ease: Linear.easeNone });
+  }
+
+  //progress timeline
+  const progressTl = new TimelineMax({
+    paused: true,
+    onUpdate: progressUpdate,
+    onComplete: loadComplete
+  });
+
+  progressTl
+    //tween the progress bar width
+    .to($('.progress span'), 1, { width: 100, ease: Linear.easeNone });
+
+  //as the progress bar width updates and grows we put the percentage loaded in the screen
+  function progressUpdate() {
+    //the percentage loaded based on the tween's progress
+    loadingProgress = Math.round(progressTl.progress() * 100);
+
+    //we put the percentage in the screen
+    $(".txt-perc").text(loadingProgress + '%');
+}
+
+  function loadComplete() {
+    // preloader out
+    var preloaderOutTl = new TimelineMax();
+
+    preloaderOutTl
+      .to($('.progress'), 0.3, { y: 100, autoAlpha: 0, ease: Back.easeIn })
+      .to($('.txt-perc'), 0.3, { y: 100, autoAlpha: 0, ease: Back.easeIn }, 0.1)
+      .set($('body'), { className: '-=is-loading' })
+      .set($('#intro'), { className: '+=is-loaded' })
+      .to($('#preloader'), 0.7, { yPercent: 100, ease: Power4.easeInOut })
+      .set($('#preloader'), { className: '+=is-hidden' })
+      .from($('#intro .header__title'), 1, { autoAlpha: 0, ease: Power1.easeOut }, '-=0.2')
+      .from($('#intro .header__tagline'), 0.7, { autoAlpha: 0, ease: Power1.easeOut }, '+=0.2')
+      .from($('#intro .nav__button'), 1, { autoAlpha: 0, ease: Power1.easeOut }, '-=0.2')
+      .from($('#intro .header__logo'), 1, { autoAlpha: 0, ease: Power1.easeOut }, '-=0.2')
+      .from($('.scroll-hint'), 0.3, { y: -20, autoAlpha: 0, ease: Power1.easeOut }, '+=0.1');
+
+    return preloaderOutTl;
+  }
+}
+
 chao.parallax = () => {
 
   /**************************************************************
@@ -116,9 +184,9 @@ chao.init = () => {
   chao.greeting();
   chao.navigation();
   chao.carousel();
+  chao.loading();
 
   if ($(window).width() >= 1150) {
-    // do some magic
     chao.parallax();
   }
 };
